450685b12be852bf8024b67623be1d6764223957
thisisdevikasunilkumar <devikasunilkumar20@gmail.com>
2025-11-12 13:47:59 +0530

    delete

--- Project/Procezo/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_register, name='login_register'),
-]
--- Project/Procezo/accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def main_page(request):
-    return render(request, 'Main Page.html')
-
-def login_register(request):
-    return render(request, 'Login Registration Form.html')
